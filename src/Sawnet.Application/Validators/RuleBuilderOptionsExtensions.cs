43437b97130536f9c5d4256bca2a8ac4b872e0e1
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-02-06 18:06:28 +0100

    add fluent validation

---
+++ src/Sawnet.Application/Validators/RuleBuilderOptionsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Sawnet.Core.Results;
+
+namespace Sawnet.Application.Validators;
+
+public static class RuleBuilderOptionsExtensions
+{
+    public static IRuleBuilderOptions<T, TProperty> WithError<T, TProperty>(
+        this IRuleBuilderOptions<T, TProperty> rule, Error error)
+    {
+        rule.WithErrorCode(error.Code);
+        rule.WithMessage(error.Message);
+        return rule;
+    }
+}
