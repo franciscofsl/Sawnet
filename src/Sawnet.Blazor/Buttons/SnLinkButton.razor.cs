6160e860064c03d0e3e99cf5429ccda8134dccf0
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-04-21 21:29:39 +0200

    add link button

---
+++ src/Sawnet.Blazor/Buttons/SnLinkButton.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace Sawnet.Blazor.Buttons;
+
+public partial class SnLinkButton
+{
+    [Parameter]
+    public string Text { get; set; }
+
+    [Parameter]
+    public string Icon { get; set; }
+    
+    [Parameter]
+    public string IconColor { get; set; } = "#737373";
+
+    [Parameter]
+    public EventCallback OnClick { get; set; } 
+
+    private async Task HandleClick()
+    {
+        if (OnClick.HasDelegate)
+        {
+            await OnClick.InvokeAsync(null);
+        }
+    }
+}
