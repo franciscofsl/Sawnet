d4dac5e47c6d1907caf6b14531ad8cc5dc24dfa4
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-05-19 11:06:34 +0200

    Add default paged result value

--- src/Sawnet.Blazor.Shared/Grpc/Filters/PagedResult.cs
+++ src/Sawnet.Blazor.Shared/Grpc/Filters/PagedResult.cs
@@ -5,5 +5,5 @@ namespace Sawnet.Blazor.Shared.Grpc.Filters;
 [DataContract]
 public class PagedResult<TItem>
 {
-    [DataMember(Order = 1)] public List<TItem> Items { get; set; }
+    [DataMember(Order = 1)] public List<TItem> Items { get; set; } = Enumerable.Empty<TItem>().ToList();
 }
