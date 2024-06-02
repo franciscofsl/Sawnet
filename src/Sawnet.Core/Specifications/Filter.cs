e5feca44f2b054e50791e3f6882f3c931027c429
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-02 13:24:51 +0200

    Filter enchancements

--- src/Sawnet.Core/Specifications/Filter.cs
+++ src/Sawnet.Core/Specifications/Filter.cs
@@ -11,7 +11,7 @@ public class Filter<TItem> where TItem : class
         _expression = item => true;
     }
 
-    public static Filter<TItem> For<TItem>() where TItem : class
+    public static Filter<TItem> Create()
     {
         return new Filter<TItem>();
     }
@@ -21,13 +21,8 @@ public class Filter<TItem> where TItem : class
         return _expression.Compile()(model);
     }
 
-    public Filter<TItem> WhereIf(bool filter, Func<TItem, bool> condition)
+    public Filter<TItem> Where(Func<TItem, bool> condition)
     {
-        if (!filter)
-        {
-            return this;
-        }
-
         var parameter = _expression.Parameters[0];
         var newCondition = Expression.Invoke(Expression.Constant(condition), parameter);
 
@@ -38,14 +33,19 @@ public class Filter<TItem> where TItem : class
 
         return this;
     }
-    
-    public Filter<TItem> OrIf(bool filter, Func<TItem, bool> condition)
+
+    public Filter<TItem> WhereIf(bool filter, Func<TItem, bool> condition)
     {
         if (!filter)
         {
             return this;
         }
 
+        return Where(condition);
+    }
+
+    public Filter<TItem> Or(Func<TItem, bool> condition)
+    {
         var parameter = _expression.Parameters[0];
         var newCondition = Expression.Invoke(Expression.Constant(condition), parameter);
 
@@ -55,5 +55,79 @@ public class Filter<TItem> where TItem : class
         );
 
         return this;
+    }
+
+    public Filter<TItem> OrIf(bool filter, Func<TItem, bool> condition)
+    {
+        if (!filter)
+        {
+            return this;
+        }
+
+        return Or(condition);
+    }
+
+    public Filter<TItem> Not(Func<TItem, bool> condition)
+    {
+        var parameter = _expression.Parameters[0];
+        var newCondition = Expression.Not(Expression.Invoke(Expression.Constant(condition), parameter));
+
+        _expression = Expression.Lambda<Func<TItem, bool>>(
+            Expression.AndAlso(_expression.Body, newCondition),
+            parameter
+        );
+
+        return this;
+    }
+
+    public Filter<TItem> NotIf(bool filter, Func<TItem, bool> condition)
+    {
+        if (!filter)
+        {
+            return this;
+        }
+
+        return Not(condition);
+    }
+
+    public Filter<TItem> And(Func<TItem, bool> condition)
+    {
+        var parameter = _expression.Parameters[0];
+        var newCondition = Expression.Invoke(Expression.Constant(condition), parameter);
+
+        _expression = Expression.Lambda<Func<TItem, bool>>(
+            Expression.AndAlso(_expression.Body, newCondition),
+            parameter
+        );
+
+        return this;
+    }
+
+    public Filter<TItem> AndIf(bool filter, Func<TItem, bool> condition)
+    {
+        if (!filter)
+        {
+            return this;
+        }
+
+        return And(condition);
+    }
+
+    public Filter<TItem> Combine(Filter<TItem> other)
+    {
+        var parameter = _expression.Parameters[0];
+        var otherBody = Expression.Invoke(other._expression, parameter);
+
+        _expression = Expression.Lambda<Func<TItem, bool>>(
+            Expression.AndAlso(_expression.Body, otherBody),
+            parameter
+        );
+
+        return this;
+    }
+
+    public Expression<Func<TItem, bool>> ToExpression()
+    {
+        return _expression;
     }
 }
