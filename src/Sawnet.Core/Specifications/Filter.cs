53de6dd3ed528d78e46a8c54d407088ba05c0238
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-02 13:00:11 +0200

    Add Filter item

---
+++ src/Sawnet.Core/Specifications/Filter.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+
+namespace Sawnet.Core.Specifications;
+
+public class Filter<TItem> where TItem : class
+{
+    private Expression<Func<TItem, bool>> _expression;
+
+    private Filter()
+    {
+        _expression = item => true;
+    }
+
+    public static Filter<TItem> For<TItem>() where TItem : class
+    {
+        return new Filter<TItem>();
+    }
+
+    public bool IsSatisfiedBy(TItem model)
+    {
+        return _expression.Compile()(model);
+    }
+
+    public Filter<TItem> If(bool filter, Func<TItem, bool> condition)
+    {
+        if (!filter)
+        {
+            return this;
+        }
+
+        var parameter = _expression.Parameters[0];
+        var newCondition = Expression.Invoke(Expression.Constant(condition), parameter);
+
+        _expression = Expression.Lambda<Func<TItem, bool>>(
+            Expression.AndAlso(_expression.Body, newCondition),
+            parameter
+        );
+
+        return this;
+    }
+}
---
+++ tests/Sawnet.Core.Tests/Specifications/FilterTest.cs
@@ -0,0 +1,41 @@
+﻿using Sawnet.Core.Specifications;
+
+namespace Sawnet.Core.Tests.Specifications;
+
+public class FilterTest
+{
+    [Fact]
+    public void Should_Satisfy_Item_Filter_If_Condition_True()
+    {
+        var item = new Item
+        {
+            Code = "New Code"
+        };
+
+        var criteria = Filter<Item>
+            .For<Item>()
+            .If(true, _ => _.Code == item.Code);
+
+        criteria.IsSatisfiedBy(item).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Should_Satisfy_Item_Filter_If_Condition_False()
+    {
+        var item = new Item
+        {
+            Code = "New Code"
+        };
+
+        var criteria = Filter<Item>
+            .For<Item>()
+            .If(false, _ => _.Code == item.Code);
+
+        criteria.IsSatisfiedBy(item).Should().BeTrue();
+    }
+
+    private class Item
+    {
+        public string Code { get; set; }
+    }
+}
